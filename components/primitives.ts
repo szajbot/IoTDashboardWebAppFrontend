9573e22c4357b4c025bf5b48ccb44fa86127665f
waszkowitobi <tobiasz.waszkowiak@wat.edu.pl>
2024-12-13 10:59:20 +0100

    Clean up project

--- components/primitives.ts
+++ components/primitives.ts
@@ -39,15 +39,3 @@ export const title = tv({
     },
   ],
 });
-
-// export const subtitle = tv({
-//   base: "w-full md:w-1/2 my-2 text-lg lg:text-xl text-default-600 block max-w-full",
-//   variants: {
-//     fullWidth: {
-//       true: "!w-full",
-//     },
-//   },
-//   defaultVariants: {
-//     fullWidth: true,
-//   },
-// });
